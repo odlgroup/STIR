9491f52b2e6a1159f64a3a5fb696e361e72ef364
Jonas Adler <jonasadl@kth.se>
2015-11-27 19:02:58 +0100

    MAINT: ran autopep8 on python tests

--- src/swig/test/python/test_IO.py
+++ src/swig/test/python/test_IO.py
@@ -30,59 +30,62 @@ try:
 except ImportError:
     pass
 
+
 def test_FloatVoxelsOnCartesianGrid(tmpdir):
     tmpdir.chdir()
     print("Creating files in ", os.getcwd())
-    origin=FloatCartesianCoordinate3D(0,1,6)
-    gridspacing=FloatCartesianCoordinate3D(1,1,2)
-    minind=Int3BasicCoordinate(3)
-    maxind=Int3BasicCoordinate(9)
-    indrange=IndexRange3D(minind,maxind)
-    image=FloatVoxelsOnCartesianGrid(indrange, origin,gridspacing)
-    image[(5,3,4)]=2
+    origin = FloatCartesianCoordinate3D(0, 1, 6)
+    gridspacing = FloatCartesianCoordinate3D(1, 1, 2)
+    minind = Int3BasicCoordinate(3)
+    maxind = Int3BasicCoordinate(9)
+    indrange = IndexRange3D(minind, maxind)
+    image = FloatVoxelsOnCartesianGrid(indrange, origin, gridspacing)
+    image[(5, 3, 4)] = 2
     print(image.shape())
-    output_format=InterfileOutputFileFormat()
+    output_format = InterfileOutputFileFormat()
     output_format.write_to_file("stir_python_test.hv", image)
-    image2=FloatVoxelsOnCartesianGrid.read_from_file('stir_python_test.hv')
-    assert image.get_voxel_size()==image2.get_voxel_size()
-    assert image.shape()==image2.shape()
-    assert get_physical_coordinates_for_bounding_box(image) == get_physical_coordinates_for_bounding_box(image2)
-    for i1,i2 in zip(image.flat(), image2.flat()):
-        assert abs(i1-i2)<.01
+    image2 = FloatVoxelsOnCartesianGrid.read_from_file('stir_python_test.hv')
+    assert image.get_voxel_size() == image2.get_voxel_size()
+    assert image.shape() == image2.shape()
+    assert get_physical_coordinates_for_bounding_box(
+        image) == get_physical_coordinates_for_bounding_box(image2)
+    for i1, i2 in zip(image.flat(), image2.flat()):
+        assert abs(i1 - i2) < .01
+
 
 def test_ProjDataInfo(tmpdir):
     tmpdir.chdir()
     print("Creating files in ", os.getcwd())
-    s=Scanner.get_scanner_from_name("ECAT 962")
-    #ProjDataInfoCTI(const shared_ptr<Scanner>& scanner_ptr,
+    s = Scanner.get_scanner_from_name("ECAT 962")
+    # ProjDataInfoCTI(const shared_ptr<Scanner>& scanner_ptr,
     #		  const int span, const int max_delta,
     #             const int num_views, const int num_tangential_poss,
     #
-    examinfo=ExamInfo();
-    projdatainfo=ProjDataInfo.ProjDataInfoCTI(s,3,6,8,6)
-    assert projdatainfo.get_scanner().get_num_rings()==32
-    projdata=ProjDataInterfile(examinfo, projdatainfo, "stir_python_test.hs")
+    examinfo = ExamInfo()
+    projdatainfo = ProjDataInfo.ProjDataInfoCTI(s, 3, 6, 8, 6)
+    assert projdatainfo.get_scanner().get_num_rings() == 32
+    projdata = ProjDataInterfile(examinfo, projdatainfo, "stir_python_test.hs")
     print(projdata.get_min_segment_num())
     print(projdata.get_max_segment_num())
-    for seg in range(projdata.get_min_segment_num(), projdata.get_max_segment_num()+1):
-        segment=projdatainfo.get_empty_segment_by_view(seg)
-        segment.fill(seg+100)
-        assert projdata.set_segment(segment)==Succeeded(Succeeded.yes)
+    for seg in range(projdata.get_min_segment_num(), projdata.get_max_segment_num() + 1):
+        segment = projdatainfo.get_empty_segment_by_view(seg)
+        segment.fill(seg + 100)
+        assert projdata.set_segment(segment) == Succeeded(Succeeded.yes)
     # now delete object such that the file gets closed and we can read it
     del projdata
 
-    projdata2=ProjData.read_from_file('stir_python_test.hs');
-    assert projdatainfo==projdata2.get_proj_data_info()
-    for seg in range(projdata2.get_min_segment_num(), projdata2.get_max_segment_num()+1):
-        # construct same segment data as above (TODO: better to stick it into a list or so)
-        segment=projdatainfo.get_empty_segment_by_view(seg)
-        segment.fill(seg+100)
+    projdata2 = ProjData.read_from_file('stir_python_test.hs')
+    assert projdatainfo == projdata2.get_proj_data_info()
+    for seg in range(projdata2.get_min_segment_num(), projdata2.get_max_segment_num() + 1):
+        # construct same segment data as above (TODO: better to stick it into a
+        # list or so)
+        segment = projdatainfo.get_empty_segment_by_view(seg)
+        segment.fill(seg + 100)
         # read from file
-        segment2=projdata2.get_segment_by_view(seg)
+        segment2 = projdata2.get_segment_by_view(seg)
         # compare
-        for i1,i2 in zip(segment.flat(), segment2.flat()):
-            assert abs(i1-i2)<.01
+        for i1, i2 in zip(segment.flat(), segment2.flat()):
+            assert abs(i1 - i2) < .01
 
 if __name__ == '__main__':
     pytest.main(str(__file__.replace('\\', '/') + ' -v'))
-
